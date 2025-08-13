69a7bdf4141f3566c08e220eddc615da078168dc
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-08-13 13:37:49 -0400

    .

---
+++ day03/rsvp/rsvp/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+using rsvp.Models;
+using System.Diagnostics;
+
+
+namespace rsvp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: store response from guest
+            //Repository.AddResponse(guestResponse);
+            //return View("Thanks", guestResponse);
+            //return View();
+
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        public IActionResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+            //return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
